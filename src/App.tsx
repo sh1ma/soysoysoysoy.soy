5a41bd5d593fef9859bf2cbcfb086442a81c8d07
sh1ma <in9lude@gmail.com>
2022-04-21 01:42:48 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import useInterval from 'use-interval'
 import './App.css'
 import useSound from 'use-sound'
